6549c672c9918673bee85e849f35d4abb2a3f26e
Jesus Machta <97565151+jesusmachta@users.noreply.github.com>
2025-09-08 19:39:56 -0400

    trying

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -4,7 +4,7 @@ export const environment = {
   production: false,
   development: true,
   firebase: firebaseConfig,
-  // Configuraciones para local development
-  apiUrl: 'http://localhost:4200',
+  // Agregar configuraciones específicas para development si es necesario
+  apiUrl: 'https://api-dev.picking-cds-ar.web.app',
   enableDebugMode: true,
 };
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,9 @@ import { firebaseConfig } from './firebase.config';
 
 export const environment = {
   production: true,
+  development: false,
   firebase: firebaseConfig,
+  // Configuraciones para production
+  apiUrl: 'https://api.picking-cds-ar.web.app',
+  enableDebugMode: false,
 };
